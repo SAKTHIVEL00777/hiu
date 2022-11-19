21af7f6321af03dd2458d6362166e8b08e36f984
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-11-19 16:26:43 +0530

    Recursion Commit

--- loop/src/com/recursion/RecursionProgram.java
+++ loop/src/com/recursion/RecursionProgram.java
@@ -11,6 +11,27 @@ public class RecursionProgram {
 		rep.print1111(4);
 		//System.out.println();
      	rep.print1234(1);
+     	int prime=rep.primeOrNot(2);
+     	if(prime==0) {
+     		System.out.println(" PRIME NO");
+     	}
+     	else {
+     		System.out.println(" NOT PRIME NO");
+     	}
+	}
+
+	private int primeOrNot(int div) {
+			int no=11;
+			int count=0;
+			if(no>div) {
+				if(no%div==0) {
+					count=count+1;
+					return count;
+				}
+				div=div+1;
+				return primeOrNot(div);
+			}
+		return count;
 	}
 
 	private int print1234(int no) {
