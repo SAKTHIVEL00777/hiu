5055d70b88a98459d07d30f581f1ab68f14bf5fd
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-11-21 11:17:56 +0530

    Letter Pattern

---
+++ loop/src/com/loop/pattern/LetterPattern.java
@@ -0,0 +1,45 @@
+package com.loop.pattern;
+
+public class LetterPattern {
+	
+	public static void main (String args[]) {
+		
+		LetterPattern lp=new LetterPattern();
+		lp.printS();
+	}
+
+	private void printS() {
+			for(int col=1;col<=5;col++) {
+				if(col==1) {
+					System.out.print(" ");
+				}
+				System.out.print("* ");
+			}
+			System.out.println();
+			for(int row=1;row<=3;row++) {
+				System.out.println("* ");
+			}
+			for(int col=1;col<=5;col++) {
+				if(col==1) {
+					System.out.print(" ");
+				}
+				System.out.print("* ");
+			}
+			System.out.println();
+			for(int row=1;row<=3;row++) {
+				for(int space=1;space<=5;space++) {
+					System.out.print("  ");
+				}
+				System.out.println("* ");
+			}
+			for(int col=1;col<=5;col++) {
+				if(col==1) {
+					System.out.print(" ");
+				}
+				System.out.print("* ");
+			}
+ }
+	
+}
+
+
