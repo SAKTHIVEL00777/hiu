7b7f1cc97c2055f718ce708e647a71188bc26c6b
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-11-22 16:06:10 +0530

    Letter Pattern

--- loop/src/com/loop/pattern/LetterPattern.java
+++ loop/src/com/loop/pattern/LetterPattern.java
@@ -5,7 +5,121 @@ public class LetterPattern {
 	public static void main (String args[]) {
 		
 		LetterPattern lp=new LetterPattern();
-		//lp.printS();  
+		//lp.printS();
+		//lp.printA();
+		//lp.printK();
+		//lp.printT();
+		//lp.printH();
+		//lp.printI();
+		//$$$$$$$$$$$$$$$$$$$$$$$$$
+		lp.printC();
+	}
+
+	private void printC() {
+			for(int col=1;col<=5;col++) {
+				if(col==1) {
+					System.out.print(" ");
+				}
+				System.out.print("* ");
+			}
+			System.out.println();
+			for(int row=1;row<=5;row++) {
+				System.out.println("* ");
+			}
+			for(int col=1;col<=5;col++) {
+				if(col==1) {
+					System.out.print(" ");
+				}
+				System.out.print("* ");
+			}
+	}
+
+	private void printI() {
+			for(int col=1;col<=5;col++) {
+				if(col==1) {
+					System.out.print(" ");
+				}
+				System.out.print("* ");
+			}
+			System.out.println();
+			for(int row=1;row<=5;row++) {
+				for(int col=1;col<=5;col++) {
+					System.out.print(" ");
+				}
+				System.out.println("*");
+			}
+			for(int col=1;col<=5;col++) {
+				if(col==1) {
+					System.out.print(" ");
+				}
+				System.out.print("* ");
+			}
+	}
+
+	private void printH() {
+			for(int row=1;row<=5;row++) {
+				System.out.print("*");
+				for(int space=1;space<=7;space++) {
+					if(row==3) {
+						System.out.print("*");
+					}
+					else
+						System.out.print(" ");
+				}
+				System.out.println("*");
+			}
+	}
+
+	private void printT() {
+			for(int col=1;col<=5;col++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+			for(int row=1;row<=5;row++) {
+				for(int col=1;col<=2;col++) {
+					System.out.print("  ");
+				}
+				System.out.println("*");
+			}
+	}
+
+	private void printK() {
+			for(int row=1;row<=5;row++) {
+				System.out.print("*");
+				for(int space=1;space<=5;space++) {
+					if(row==1 && space==5 || row==2 && space==3 ||row==3 && space==2 ||row==4 && space==3||row==5 && space==5) {
+						System.out.print("* ");
+					}
+					else {
+						System.out.print(" ");
+					}
+				}
+				System.out.println();
+			}
+	}
+
+	private void printA() {
+			for(int col=1;col<=5;col++) {
+				if(col==1) {
+					System.out.print(" ");
+				}
+				System.out.print("* ");
+			}
+			System.out.println();
+			for(int row=1;row<=5;row++) {
+				System.out.print("*");
+				for(int space=1;space<=4;space++) {
+					if(space==1) {
+						System.out.print(" ");
+					}
+					if(row==3) {
+						System.out.print("* ");
+					}
+					else
+						System.out.print("  ");
+				}
+				System.out.println("*");
+			}
 	}
 
 	private void printS() {
@@ -36,9 +150,9 @@ public class LetterPattern {
 				if(col==1) {
 					System.out.print(" ");
 				}
-				System.out.print("* ");
+				System.out.print("*  ");
 			}
- }
+     }
 	
 }
 
