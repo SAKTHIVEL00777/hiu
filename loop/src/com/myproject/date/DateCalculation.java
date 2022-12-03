696cef14d6fb240ff9291f1e8002222ba8ead973
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-12-03 22:22:31 +0530

    DateCaluculation for DOB

---
+++ loop/src/com/myproject/date/DateCalculation.java
@@ -0,0 +1,31 @@
+package com.myproject.date;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class DateCalculation {
+
+	public static void main(String[] args) throws ParseException {
+
+		int sec=60;
+		int mins=60;
+		int hrs=24;
+		int yrs=365;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your date of birth(dd/mm/yyyy)");
+		String birth=sc.next();
+        Date d=new Date();	
+        SimpleDateFormat sd=new SimpleDateFormat("dd/mm/yy");
+        String current=sd.format(d);
+        Date d1=sd.parse(birth);
+        Date d2=sd.parse(current);
+        long time=d2.getTime()-d1.getTime();
+        long year=(time/(1000l*sec*mins*hrs*yrs));
+        long day=(time/(1000*sec*mins*hrs)%yrs);
+        System.out.println(year+" years "+ day+" days");
+
+	}
+
+}
