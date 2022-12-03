d26ba76ad5ca788b46b9dca4c8af94687bee1b1e
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-12-04 05:05:21 +0530

    EB

---
+++ loop/src/com/myproject/eb/EbCalculation.java
@@ -0,0 +1,13 @@
+package com.myproject.eb;
+
+import java.util.Scanner;
+
+public class EbCalculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+
+	}
+
+}
