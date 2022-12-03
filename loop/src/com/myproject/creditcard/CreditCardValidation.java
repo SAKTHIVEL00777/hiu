5bb1dbf987e5fa2ffd33b1c72a9c231088eda6f2
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-12-04 05:02:00 +0530

    credit card

---
+++ loop/src/com/myproject/creditcard/CreditCardValidation.java
@@ -0,0 +1,13 @@
+package com.myproject.creditcard;
+
+import java.util.Scanner;
+
+public class CreditCardValidation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+
+	}
+
+}
