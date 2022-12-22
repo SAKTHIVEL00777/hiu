e8d7fc5f62fade307cb0b5dcfcee34603eef955c
SAKTHIVEL00777 <sakthi040797@gmail.com>
2022-12-22 17:34:39 +0530

    Thread

--- loop/src/com/thread/ThreadChild.java
+++ loop/src/com/thread/ThreadChild.java
@@ -1,12 +1,15 @@
 package com.thread;
 
-public class ThreadChild  extends Thread{
+public class ThreadChild extends Thread{
 
 	public void run() {
 		
-		for(int i=1;i<=5;i++) 
-		{
-		System.out.println("Thread child"+ i);
+		System.out.println("Before start---->"+this.getState());
+
+		for(int i=1;i<=5;i++){
+			
+			System.out.println("Thread Child"+ i);
 		}
 	}
+
 }
--- loop/src/com/thread/ThreadDemo.java
+++ loop/src/com/thread/ThreadDemo.java
@@ -3,14 +3,17 @@ package com.thread;
 public class ThreadDemo {
 
 	public static void main(String[] args) {
+		 
 		ThreadChild tc=new ThreadChild();
+		System.out.println("Before start---->"+tc.getState());
 		//tc.run();
-		System.out.println(tc.getState());
-		tc.start();   //it starts the thread
-		
-		for(int i=1;i<=5;i++) 
-		{
-		System.out.println("Thread Demo"+ i);
+		tc.start();  //it starts the thread
+		System.out.println("After start---->"+tc.getState());
+
+		for(int i=1;i<=5;i++) {
+			
+			System.out.println("Thread Demo"+ i);
 		}
+		
 	}
 }
