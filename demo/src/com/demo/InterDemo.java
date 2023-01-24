8b7cb7310f0b6ced788a4f79507134d72655dd13
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-01-24 17:26:37 +0530

    InterDemo

--- demo/src/com/demo/InterDemo.java
+++ demo/src/com/demo/InterDemo.java
@@ -1,12 +1,12 @@
 package com.demo;
 
 public interface InterDemo {
-	 String ADDRESS="TRICHY"; ///by default static and final
-	void add();  	///by default abstract method
+	 String ADDRESS="TRICHY"; //by default static and final
+	void add();  	//by default abstract method
 	void multiple();
 	
 	default void show() {
-		System.out.println("interface show");
+		System.out.println("INTERFACE SHOW");
 	}
 	static void subtract() {
 		System.out.println(20-10);
