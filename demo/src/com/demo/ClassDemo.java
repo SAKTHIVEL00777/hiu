411543bbaf94fa00ace5f7b20435ca49810930e1
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-03-04 13:59:57 +0530

    ClassDemo

--- demo/src/com/demo/ClassDemo.java
+++ demo/src/com/demo/ClassDemo.java
@@ -7,10 +7,11 @@ public class ClassDemo implements InterDemo{
 	}
 	
 	
-	  ClassDemo(int a){
-	  
-	  }
-	 
+	/*
+	 * ClassDemo(int a){
+	 * 
+	 * }
+	 */
 	 public static void main(String[] args) {
 		InterDemo cd=new ClassDemo();
 		cd.add();
