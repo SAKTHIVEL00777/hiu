e5737befebf32c814a9927aa86b390d835cd77e9
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-03-13 12:54:04 +0530

    Variables

---
+++ demo/src/com/test/Test.java
@@ -0,0 +1,21 @@
+package com.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+             Test t=new Test();
+             t.variable();
+	}
+
+	private void variable() {
+		// TODO Auto-generated method stub
+		int a=25;
+		String name="DHONI";
+		
+		System.out.println(a);
+		System.out.println(name);
+		
+	}
+
+}
