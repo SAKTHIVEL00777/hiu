d74993cc48677099a50894d7fea1e124bc69b822
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-03-14 14:45:42 +0530

    Variables

--- demo/src/com/test/Test.java
+++ demo/src/com/test/Test.java
@@ -2,6 +2,8 @@ package com.test;
 
 public class Test {
 
+	 int aa=100;
+	 //static String city="TRICHY";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
              Test t=new Test();
@@ -15,7 +17,10 @@ public class Test {
 		
 		System.out.println(a);
 		System.out.println(name);
+		System.out.println(aa);
+		//System.out.println(city);
 		
 	}
+	
 
 }
