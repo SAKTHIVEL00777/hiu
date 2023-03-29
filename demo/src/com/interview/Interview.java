1771396d111aea5bc45274a2f6739b416002ebe6
SAKTHIVEL00777 <sakthi040797@gmail.com>
2023-03-29 13:29:19 +0530

    code submit

--- demo/src/com/interview/Interview.java
+++ demo/src/com/interview/Interview.java
@@ -8,7 +8,27 @@ public class Interview {
 		Interview in=new Interview();
 		//in.reverse();
 		//in.swappingNo();
-		in.vowels("kavinkumar");
+		//in.vowels("kavinkumar");
+		in.primeNo();
+		
+	}
+	private void primeNo() {
+		// TODO Auto-generated method stub
+		int no=17;
+		int div=2;
+		int count=0;
+		while(div<no) {
+			if(no%div==0) {
+				count=count+1;
+			}
+			div=div+1;
+		}
+		if(count==0) {
+			System.out.println(no+" is prime number");
+		}
+		else {
+			System.out.println(no+" is not prime number");
+		}
 		
 	}
 	
